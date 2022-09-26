a40fde8a6f3b3aea4a65c237274b6188e30a6232
beraatt <113882776+beraatt@users.noreply.github.com>
2022-09-26 11:24:22 +0300

    rastgele kullanıcı ekleme

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,15 +24,23 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        User::insert([
+            'name'=>'Beraat Tekin',
+            'email'=>'beraattekinn@gmail.com',
+            'email_verified_at'=>now(),
+            'type'=>'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
        
-        $types=['admin','user'];
+      $types=['admin','user'];
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'type'=>$types[rand(0,1)],
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'type'=>$types[rand(0,1)]
         ];
     }
 
