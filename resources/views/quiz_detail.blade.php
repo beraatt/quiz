91b11d28d6702359378bae86b5deac0bab8d133d
beraatt <113882776+beraatt@users.noreply.github.com>
2022-10-07 08:52:38 +0300

    s

--- resources/views/quiz_detail.blade.php
+++ resources/views/quiz_detail.blade.php
@@ -75,7 +75,7 @@
                                             <img class="h-8 w-8  rounded-full"
                                                 src="{{ $result->user->profile_photo_url }}">
                                             <span
-                                                @if (auth()->user()->id == $result->user_id) class="text-teal-600" @endif >{{ $result->user->name }}</span>
+                                                @if (auth()->user()->id == $result->user_id) class="text-teal-600" @endif>{{ $result->user->name }}</span>
                                             <span
                                                 class="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-800 float-right ">{{ $result->point }}</span>
                                         </li>
@@ -87,13 +87,16 @@
                     @endif
                 </div>
                 <div class="col-md-8">
-                    {{ $quiz->description }}</p>
+                    {{ $quiz->description }}
+                    </p>
                     @if ($quiz->my_result)
                         <a href="{{ route('quiz.join', $quiz->slug) }}" class="btn btn-warning btn-block btn-sm">Quizi
                             Görüntüle</a>
-                    @elseif($quiz->finished_at>now())
-                        <a href="{{ route('quiz.join', $quiz->slug) }}" class="btn btn-primary btn-block btn-sm">Quize
-                            Katıl</a>
+                        @else ($quiz->finished_at > now())
+                            <a href="{{ route('quiz.join', $quiz->slug) }}"
+                                class="btn btn-primary btn-block btn-sm">Quize
+                                Katıl</a>
+
                     @endif
                 </div>
             </div>
